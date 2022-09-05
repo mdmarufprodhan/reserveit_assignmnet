f68fe8018b55e5f5e60788c50fbd8fa0d4b93ad5
mdmarufprodhan <marufprodhan463@gmail.com>
2022-09-05 23:06:35 +0600

    Initial Commit

---
+++ demoAssignmnet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demoAssignmnet
+//
+//  Created by Md Maruf Prodhan on 5/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demoAssignmnet/demoAssignmnetApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoAssignmnetApp.swift
+//  demoAssignmnet
+//
+//  Created by Md Maruf Prodhan on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct demoAssignmnetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
